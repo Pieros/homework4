a8bf46112df3fd68ab9f236732a7f06d2b837cc3
Pieros <p_boy55_sd41@hotmail.com>
2014-01-17 22:53:32 +0700

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,8 @@
+#ณัฐนันท์ ประพันธ์ศิริ 5310613194
+#นัฐพร กำศิริพิมาน 5310611065
+
 class Movie < ActiveRecord::Base
+	#สร้างตัวแปร instanceสำหรับ class Movie เป็น Array ที่เก็บ rating ไว้
 def self.all_ratings
 	['G','PG','PG-13','R']
 end
